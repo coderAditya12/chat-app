9ecf076fe011200db8f7b6dcfc5d4466153bd991
Aditya Gupta <realadityagupta2@gmail.com>
2025-07-12 16:10:53 +0530

    eslint remove

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -10,7 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // Disable all ESLint rules
+  {
+    rules: {},
+    // Remove all extends to disable all rules
+    // ...compat.extends("next/core-web-vitals", "next/typescript"),
+  },
 ];
 
 export default eslintConfig;
