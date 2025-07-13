24240c8cfaed305f2b624b06dd43a0f2fe703767
Aditya Gupta <realadityagupta2@gmail.com>
2025-07-13 11:28:12 +0530

    final frontend deploy

--- client/src/components/CallButton.tsx
+++ client/src/components/CallButton.tsx
@@ -2,7 +2,7 @@ import { VideoIcon } from "lucide-react";
 
 function CallButton({ handleVideoCall }: { handleVideoCall: () => void }) {
   return (
-    <div className="p-3 border-b flex items-center justify-end max-w-7xl mx-auto w-full absolute top-0">
+    <div className="p-3 border-b flex items-center justify-end max-w-full mx-auto w-full absolute top-0">
       <button
         onClick={handleVideoCall}
         className="btn btn-success btn-sm text-white"
